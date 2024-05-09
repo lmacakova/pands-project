c862471b72d023f71439edf96dfc1e04d6d13f39
lmacakova <l.macakova.sk@gmail.com>
2024-05-09 11:02:03 +0100

    README

--- creating_csv.py
+++ creating_csv.py
@@ -2,11 +2,9 @@ import pandas as pd
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 
-# Define the column names
+# Naming the columns
 col_names = ["sepal_length_in_cm", "sepal_width_in_cm", "petal_length_in_cm", "petal_width_in_cm", "species"]
 
-# Read data from URL
+# Creating csv file
 iris = pd.read_csv(url, names=col_names)
 iris.to_csv('iris.csv', index=False)
-
-
